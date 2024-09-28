48588b4374e71e8f86fd2513b343e9965411df7e
marwakaabar <marwa.kaabar@esprit.tn>
2024-09-28 21:30:40 +0100

    test

--- resources/views/Front/welcome.blade.php
+++ resources/views/Front/welcome.blade.php
@@ -4,14 +4,21 @@ hey still in progress
 
 
 
-   <!-- Logout Form -->
-   <div class="mt-4">
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <a href="{{ route('logout') }}"
-           onclick="event.preventDefault(); this.closest('form').submit();"
+<!-- Logout Form -->
+<div class="mt-4">
+    @guest
+        <a href="{{ route('login') }}"
            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-            {{ __('Log Out') }}
+            {{ __('Log In') }}
         </a>
-    </form>
+    @else
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <a href="{{ route('logout') }}"
+               onclick="event.preventDefault(); this.closest('form').submit();"
+               class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                {{ __('Log Out') }}
+            </a>
+        </form>
+    @endguest
 </div>
